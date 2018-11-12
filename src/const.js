796b2b0ffa9a0a6b6d78a0aa3116f94f475649e7
高天阳 <13683265113@163.com>
2018-11-12 10:36:21 +0800

    const

--- src/const.js
+++ src/const.js
@@ -1,4 +1,6 @@
-// ES5 中常量的写法
+/**
+ * ES5 中常量的写法
+ */
 
 /**
  * value为Windows的属性，writable是代表是否只读
@@ -7,25 +9,28 @@ Object.defineProperty(window, "PI2", {
     value: 3.1415926,
     writable: false,
 })
-
-/**
- * 下面方法虽然可以声明PI3为window的属性，但并不是只读属性
- */
-// window.PI3 = {
-//   value: 3.1415926,
-//   writable: false
-// }
-// console.log(window)
-// console.log(window.PI3)
 /**
  * 无法对该属性进行重新赋值操作
  */
 // window.PI2 = 4
 console.log(window.PI2)
 
-// ES6 的常量写法
+/**
+ * 下方法虽然可以声明PI3为window的属性，但并不是只读属性
+ */
+// window.PI3 = {
+//   value: 3.1415926,
+//   writable: false
+// }
+// console.log(window)
+// window.PI3 = 4
+// console.log(window.PI3)
+
+/**
+ * ES6 的常量写法
+ */
 
 const PI = 3.1415926
+// PI = 4
 console.log(PI)
 
-// PI = 4
